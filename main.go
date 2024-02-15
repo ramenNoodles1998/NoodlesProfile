a1574b055263fc93e75efcf761a0f869503acb7d
Roman Meredith <ROMANMEREDITH@boisestate.edu>
2024-02-14 20:12:04 -0700

    testing data and nested templates.

--- main.go
+++ main.go
@@ -6,18 +6,58 @@ import (
 	"html/template"
 )
 
+type Template struct {
+	template *template.Template
+	data map[string]interface{}
+}
+
 func main() {
-	var tmpl, err = template.New("body").ParseFiles("body.html")
-	tmpl.New("index").ParseFiles("index.html")
+	var templates []Template 
+	renderer(&templates)
+
+	http.HandleFunc("/", func (w http.ResponseWriter, r *http.Request) {
+		for i := 0; i < len(templates); i++ { 
+			var tmpl = templates[i]
+			tmpl.template.Execute(w, tmpl.data)
+		} 
+	})
+
+	http.ListenAndServe(":80", nil)
+}
+
+func renderer(templates *[]Template) {
+
+	var tmpl, err = template.New("index").ParseFiles("index.html")
+
+	if err != nil {
+		fmt.Printf("Error parsing html. %#v", err.Error())
+	} else {
+		fmt.Printf("we gucci")
+	}
+	
+	var template = Template {
+		template: tmpl,
+		data: map[string]interface{}{
+			"Word": "Index",
+		},
+	}
+
+	*templates = append(*templates, template)
+
+	tmpl, err = tmpl.New("body").ParseFiles("body.html")
+
 	if err != nil {
 		fmt.Printf("Error parsing html. %#v", err.Error())
 	} else {
 		fmt.Printf("we gucci")
 	}
 
-	http.HandleFunc("/", func (w http.ResponseWriter, r *http.Request) {
-		tmpl.Execute(w, nil)
-	})
+	template = Template {
+		template: tmpl,
+		data: map[string]interface{}{
+			"Word": "Body",
+		},
+	}
 
-	http.ListenAndServe(":80", nil)
+	*templates = append(*templates, template)
 }
