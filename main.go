756203f56171a957ab24bf1b810c65055e815866
Roman Meredith <ROMANMEREDITH@boisestate.edu>
2024-02-15 00:13:06 -0700

    realized im dog shit

--- main.go
+++ main.go
@@ -1,91 +1,30 @@
 package main
 
 import (
-	"fmt"
 	"html/template"
 	"net/http"
 )
 
-type Template struct {
-	template *template.Template
-	data map[string]string
-}
+// type Template struct {
+// 	template *template.Template
+// 	data map[string]string
+// }
 
-var TEMPLATE_MAP = map[string]map[string]string {
-	"index": {
-		"Word": "Index",
-	},
-	"body": {
-		"Word": "Body",
-	},
-}
+// var TEMPLATE_MAP = map[string]map[string]string {
+// 	"index": {
+// 		"Word": "Index",
+// 	},
+// 	"body": {
+// 		"Word": "Body",
+// 	},
+// }
 
 func main() {
-	var templates []Template 
-	renderer(&templates)
+	var t = template.Must(template.ParseGlob("templates/*.gohtml"))
 
 	http.HandleFunc("/", func (w http.ResponseWriter, r *http.Request) {
-		for i := 0; i < len(templates); i++ { 
-			var tmpl = templates[i]
-			tmpl.template.Execute(w, tmpl.data)
-		} 
+		t.ExecuteTemplate(w, "index.gohtml", "Hello")
 	})
 
 	http.ListenAndServe(":8080", nil)
 }
-
-func renderer(templates *[]Template) {
-	for k, v := range TEMPLATE_MAP { 
-		fmt.Println(k)
-		fmt.Println(v)
-		var tmpl, err = template.New(k).ParseFiles(k + ".html")
-
-		if err != nil {
-			fmt.Printf("Error parsing html. %#v\n", err.Error())
-		} else {
-			fmt.Println("we gucci")
-		}
-		
-		var t = Template {
-			template: tmpl,
-			data: v,
-		}
-
-		*templates = append(*templates, t)
-	}
-
-	// var tmpl, err = template.New("index").ParseFiles("index.html")
-
-	// if err != nil {
-	// 	fmt.Printf("Error parsing html. %#v", err.Error())
-	// } else {
-	// 	fmt.Printf("we gucci")
-	// }
-	
-	// var template = Template {
-	// 	template: tmpl,
-	// 	data: map[string]string{
-	// 		"Word": "Index",
-	// 	},
-	// }
-
-	// *templates = append(*templates, template)
-
-	// tmpl, err = tmpl.New("body").ParseFiles("body.html")
-
-	// if err != nil {
-	// 	fmt.Printf("Error parsing html. %#v", err.Error())
-	// } else {
-	// 	fmt.Printf("we gucci")
-	// }
-
-	// template = Template {
-	// 	template: tmpl,
-	// 	data: map[string]string{
-	// 		"Word": "Body",
-	// 	},
-	// }
-
-	// *templates = append(*templates, template)
-}
-
