a56ac1db4ceed86253653988480982a4ac7b9f3d
Roman Meredith <ROMANMEREDITH@boisestate.edu>
2024-02-15 19:33:45 -0700

    active buttons implementation needs clean up but idea there

--- main.go
+++ main.go
@@ -8,17 +8,54 @@ import (
 
 func main() {
 	var t = template.Must(template.ParseGlob("templates/*.html"))
+	var x = map[string]interface{} {
+		"AboutMeButtonClass":"p-3 hover:bg-cyan-700 hover:text-2xl rounded-t text-xl bg-cyan-700 cursor-pointer",
+		"PortfolioButtonClass":"p-3 hover:bg-cyan-700 hover:text-2xl rounded-t text-xl bg-cyan-900 cursor-pointer",
+		"ContactMeButtonClass":"p-3 hover:bg-cyan-700 hover:text-2xl rounded-t text-xl bg-cyan-900 cursor-pointer",
+		"AboutMe": true,
+		"Portfolio": false,
+		"ContactMe": false,
+	}
 
 	http.HandleFunc("/", func (w http.ResponseWriter, r *http.Request) {
 		fmt.Printf("template executed")
-		t.ExecuteTemplate(w, "index.html", nil)
+		t.ExecuteTemplate(w, "index.html", x)
 	})
 
 	http.HandleFunc("/clicked", func (w http.ResponseWriter, r *http.Request) {
-		//TODO: add query params for each button.
-		var htmlStr = "<div>Hello</div>"
-		var tmpl, _ = template.New("t").Parse(htmlStr)
-		tmpl.Execute(w, nil)
+		var buttonName = r.URL.Query().Get("button")
+		var d map[string]interface{}
+
+		if buttonName == "about-me"{
+			d = map[string]interface{} {
+				"AboutMeButtonClass":"p-3 hover:bg-cyan-700 hover:text-2xl rounded-t text-xl bg-cyan-700 cursor-pointer",
+				"PortfolioButtonClass":"p-3 hover:bg-cyan-700 hover:text-2xl rounded-t text-xl bg-cyan-900 cursor-pointer",
+				"ContactMeButtonClass":"p-3 hover:bg-cyan-700 hover:text-2xl rounded-t text-xl bg-cyan-900 cursor-pointer",
+				"AboutMe": true,
+				"Portfolio": false,
+				"ContactMe": false,
+			}
+		} else if buttonName == "portfolio"{
+			d = map[string]interface{} {
+				"AboutMeButtonClass":"p-3 hover:bg-cyan-700 hover:text-2xl rounded-t text-xl bg-cyan-900 cursor-pointer",
+				"PortfolioButtonClass":"p-3 hover:bg-cyan-700 hover:text-2xl rounded-t text-xl bg-cyan-700 cursor-pointer",
+				"ContactMeButtonClass":"p-3 hover:bg-cyan-700 hover:text-2xl rounded-t text-xl bg-cyan-900 cursor-pointer",
+				"AboutMe": false,
+				"Portfolio": true,
+				"ContactMe": false,
+			}
+		} else if buttonName == "contact-me"{
+			d = map[string]interface{} {
+				"AboutMeButtonClass":"p-3 hover:bg-cyan-700 hover:text-2xl rounded-t text-xl bg-cyan-900 cursor-pointer",
+				"PortfolioButtonClass":"p-3 hover:bg-cyan-700 hover:text-2xl rounded-t text-xl bg-cyan-900 cursor-pointer",
+				"ContactMeButtonClass":"p-3 hover:bg-cyan-700 hover:text-2xl rounded-t text-xl bg-cyan-700 cursor-pointer",
+				"AboutMe": false,
+				"Portfolio": false,
+				"ContactMe": true,
+			}
+		}
+
+		t.ExecuteTemplate(w, "index.html", d)
 	})
 
 	http.ListenAndServe(":8080", nil)
